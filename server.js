d12c04bcecda3c48b0b28c041e46c56cd2ef7b74
Krishna Tent House <chamanrathor09@gmail.com>
2026-02-11 23:31:22 +0530

    fixed render port issue

--- server.js
+++ server.js
@@ -90,6 +90,8 @@ app.post("/create-order", async (req, res) => {
 
 /* ================= START SERVER ================= */
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
